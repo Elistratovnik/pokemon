55f348a9176bd3ef772b31212acb003d8e833d2e
ElistratovNik <elistratovnik@yandex.ru>
2020-10-05 12:53:41 +0300

    Updates

---
+++ precache-manifest.41d37f725cd0cadbf10308756c9c76cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79558067bcd72d2cc755c495aa5500b5",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "7ac4ff621b8049d6fa90",
+    "url": "/pokemon/static/css/main.98e103c2.chunk.css"
+  },
+  {
+    "revision": "5c554260cfdd275967cc",
+    "url": "/pokemon/static/js/2.92493ed7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemon/static/js/2.92493ed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac4ff621b8049d6fa90",
+    "url": "/pokemon/static/js/main.b7ee9b48.chunk.js"
+  },
+  {
+    "revision": "b931b09f9078cae05f94",
+    "url": "/pokemon/static/js/runtime-main.89aedc36.js"
+  },
+  {
+    "revision": "cd6175214dd16ec27642a937fd572e96",
+    "url": "/pokemon/static/media/Pokemon.cd617521.ttf"
+  },
+  {
+    "revision": "c8273aa276411ce77bd41c9400908917",
+    "url": "/pokemon/static/media/background.c8273aa2.jpeg"
+  }
+]);
